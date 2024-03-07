e55e731fff21e99489397b3dd92554e0a52404fb
Ingrid Nascimento <ingcnasc@gmail.com>
2024-03-07 11:20:45 -0300

    refresh button enabled

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -17,9 +17,33 @@ function sortear() {
     }
 
     let imprimirResultado = document.getElementById('resultado');
-    imprimirResultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`
+    imprimirResultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`;
+
+    ativarReiniciar();
 }
 
 function sorteioNumeroAleatorio(minimo, maximo) {
     return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+}
+
+function ativarReiniciar() {
+    let botao = document.getElementById('btn-reiniciar');
+
+    if(botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+
+    ativarReiniciar();
+
 }
