fcb2292143b4c0cfdc369b390c394db5a8e66e4c
Ingrid Nascimento <ingcnasc@gmail.com>
2024-03-11 11:49:02 -0300

    count games rented

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,15 +1,33 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados = ${jogosAlugados}.`);
+}
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
+
     if(imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
+        alert('O jogo foi devolvido!');
+        jogosAlugados--;
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
-    }
+        jogosAlugados++;
+    };
+
+    contarEExibirJogosAlugados();
+
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
