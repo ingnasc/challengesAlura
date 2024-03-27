8b4b56fa032fe3338bb47fa4e254c9ad8abdc8a3
Ingrid Nascimento <ingcnasc@gmail.com>
2024-03-27 13:43:18 -0300

    conclusion

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,11 +1,28 @@
-function adicionar() {
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
+let total;
+limpar();
 
-    alert(nomeProduto.value);
-    alert(quantidade.value);
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let subtotal = quantidade * valorUnitario;
+
+    let listaCarrinho = document.getElementById('lista-produtos');
+    listaCarrinho.innerHTML = listaCarrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${subtotal}</span>
+    </section>`
+
+    total = total + subtotal;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$${total}`;
+
+    document.getElementById('quantidade').value = '';
+
 }
 
 function limpar() {
-    
+    total = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = 'R$0,00';
 }
