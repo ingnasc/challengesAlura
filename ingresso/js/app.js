212decc7de3cf9e17d8696c98742360f088f73ec
Ingrid Nascimento <ingcnasc@gmail.com>
2024-04-01 09:40:43 -0300

    recovery values of name and quantity

---
+++ ingresso/js/app.js
@@ -0,0 +1,14 @@
+function comprar() {
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidadeIngresso = document.getElementById('qtd').value;
+
+    alert(tipoIngresso);
+    alert(quantidadeIngresso);
+}
+
+
+
+//botao comprar
+//resgatar elementos ingresso e quantidade
+//limite na quantidade de compra
+//subtrair depois de comprado e mostrar
