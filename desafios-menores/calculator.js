fb38a92a6d99d97f557447e18be90ff0233138ae
Ingrid Nascimento <ingcnasc@gmail.com>
2024-04-01 11:36:07 -0300

    new challenges

---
+++ desafios-menores/calculator.js
@@ -0,0 +1,40 @@
+//Escreva um programa de calculadora simples com funções separadas para adição, subtração, multiplicação e divisão. Utilize um bloco condicional para determinar qual função executar com base em uma variável que representa a operação desejada.
+
+function adicao(a, b){
+    return a + b;
+}
+
+function subtracao(a, b){
+    return a - b;
+}
+
+function multiplicacao(a, b){
+    return a * b;
+}
+
+function divisao(a, b){
+    if (b !== 0){
+        return a / b;
+    } else {
+        return 'Erro: divisão por zero.';
+    }
+}
+
+function calculadora(operacao, a, b){
+    switch(operacao){
+        case adicao:
+            return adicao(a, b);
+
+        case subtracao:
+            return subtracao(a, b);
+            
+        case multiplicacao:
+            return multiplicacao(a, b);
+            
+        case divisao:
+            return divisao(a, b);
+
+        default:
+            return 'Operação Inválida.';
+    }
+}
---
+++ desafios-menores/function-string-number.js
@@ -0,0 +1,5 @@
+//Crie uma função que aceite uma string como parâmetro, utilize a função parseInt para converter essa string em um número inteiro e retorne o resultado.
+
+function converterParaNumero(valorString){
+    return parseInt(valorString);
+}
