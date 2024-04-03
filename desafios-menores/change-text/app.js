41e060697fefd693c7078d78c6e06b1b96d0977a
Ingrid Nascimento <ingcnasc@gmail.com>
2024-04-03 19:57:00 -0300

    change simple text

---
+++ desafios-menores/change-text/app.js
@@ -0,0 +1,6 @@
+//Tenha um elemento HTML na página (por exemplo, um parágrafo) e utilize JavaScript para modificar seu conteúdo usando a propriedade textContent
+
+function modificarTexto() {
+    let paragrafo = document.getElementById('paragrafo');
+    paragrafo.textContent = 'Texto alterado.';
+}
