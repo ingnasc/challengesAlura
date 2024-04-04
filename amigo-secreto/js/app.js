5aecfeaa1fe483c3989689c89a4c7b10f386bc9b
Ingrid Nascimento <ingcnasc@gmail.com>
2024-04-04 12:29:59 -0300

    finalisation of functions

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,7 +1,9 @@
-//adicionar nomes numa lista, receber os nomes, recuperar os nomes
+let listaArray = [];
+
 function adicionar() {
     let nomeAmigo = document.getElementById('nome-amigo');
     let listaAmigos = document.getElementById('lista-amigos');
+    listaArray.push(nomeAmigo.value);
     
     if(listaAmigos.textContent == '') {
         listaAmigos.textContent = nomeAmigo.value;
@@ -13,12 +15,34 @@ function adicionar() {
     
 }
 
-//sortear os nomes da lista e apresentar na tela
 function sortear() {
-    let listaSorteio = document.getElementById('lista-sorteio');
+    embaralha(listaArray);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for(let i = 0; i < listaArray.length; i++) {
+        if(i == listaArray.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + listaArray[i] + ' ---> ' + listaArray[0];
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + listaArray[i] + ' ---> ' + listaArray[i + 1] + '<br>';
+        }
+    }
+
 }
 
-//zerar todas as informações
-function reiniciar() {
+//Fisher–Yates
+function embaralha(lista) { 
+    for (let indice = lista.length; indice; indice--) {
 
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    listaArray = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
