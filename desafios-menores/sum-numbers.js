e92751919eba4b569f1c9c9e2574f5fbda60648d
Ingrid Nascimento <ingcnasc@gmail.com>
2024-04-04 10:53:10 -0300

    challenge template string

---
+++ desafios-menores/sum-numbers.js
@@ -0,0 +1,10 @@
+//Faça a soma de duas variáveis numéricas e apresente no console com uma mensagem amigável em Template String.
+
+let numero1 = 10;
+let numero2 = 20;
+
+let soma = numero1 + numero2;
+
+let mensagem = `A soma entre o número ${numero1} e o número ${numero2} é ${soma}.`;
+
+console.log(mensagem);
