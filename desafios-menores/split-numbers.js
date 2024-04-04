ef52b73855e6df679eb113a1c7921b9eadf3fb9c
Ingrid Nascimento <ingcnasc@gmail.com>
2024-04-04 11:06:11 -0300

    challenge split numbers

---
+++ desafios-menores/split-numbers.js
@@ -0,0 +1,6 @@
+//Receba uma string contendo números separados por vírgulas. Utilize split() para separar os números e exibi-los no console.
+
+let numeros = '23, 45, 98, 102, 2';
+let numerosSeparados = numeros.split(',');
+
+console.log(numerosSeparados);
